8e5f5ed82f7b3274b2e3a94bf617f72e34e59211
karam <karampanah927@gmail.com>
2024-11-18 22:19:42 +0100

    CTR over Brand category calculated Field Creation

---
+++ Data_Cleansing15.sql
@@ -0,0 +1,32 @@
+/*
+alter table car_ads
+	add ctr_rank_over_category int
+*/
+	go
+--------------------- ctr rank over brand_category------------------------------
+
+
+
+	Create Procedure Data_Cleansing13
+	as
+	begin
+	--begin tran
+		;with ranked_ctr_brandcat_cte as (
+			select 
+				ad_id,
+				brand_category,
+				ctr,
+				dense_rank() over (partition by brand_category 	order by ctr desc) 	as ctr_rank_over_category
+			from car_ads
+		)
+				update a
+				set ctr_rank_over_category=b.ctr_rank_over_category
+				from car_ads  a
+				inner join ranked_ctr_brandcat_cte b  on a.ad_id =b.ad_id
+
+		--select * from car_ads
+	--rollback
+	end
+
+
+
